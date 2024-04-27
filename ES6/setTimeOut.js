b9ed166f08ab7a5ec757e30d5639ab0ac819787a
Rakibhasan-programmer <rakibulhasan.seu.cse@gmail.com>
2024-04-27 23:13:23 +0600

    setTimeOut and setInterval

---
+++ ES6/setTimeOut.js
@@ -0,0 +1,25 @@
+setTimeout(() => {
+    console.log("Hi");
+}, 1000)
+
+// it will print value after defined second only once
+
+console.log("Hey");
+
+// for(let i=1; i<=10000; i++){
+//     console.log();
+// }
+
+
+setTimeout(() => {
+    console.log("Hello");
+}, 10)
+
+
+// 
+setInterval(() => {
+    console.log("Rakib");
+}, 1000)
+
+
+
