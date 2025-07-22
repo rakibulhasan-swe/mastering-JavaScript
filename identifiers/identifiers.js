9ee1f7b77c7fe5ebc7396cfcd943939ad59f9bbb
Md. Rakibul Hasan <rakibulhasan.seu.cse@gmail.com>
2025-07-22 23:38:43 +0600

    identifiers

---
+++ identifiers/identifiers.js
@@ -0,0 +1,21 @@
+/*
+Identifier Rules - All JavaScript variables must be identified with unique names
+
+    1. Names can contain letters, digits, underscores and dollar signs. (no spacing).
+    2. Names must begin with a letter.
+    3. Names can also begin with $ (dollar sign) and _ (underscore).
+    4. Names are case sensitive like n and N are different.
+    5. Reserve words can not be used for variable name. (JS keywords like loop, if etc)
+*/
+
+// Valid identifiers
+let name;
+let $dollar;
+let _underscore;
+let x;
+let X;
+
+
+// invalid number
+// let 12hello;
+// let &z;
